b4de9cbdfda683cdb223ae8e74ab98029ffa7ef9
vedas12345 <vedp805@gmail.com>
2016-05-20 23:41:00 +0530

    Commit
    Initial One

---
+++ Assets/move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+    //public GameObject GO1;
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+        //GO1 = GameObject.Find("Name_Of_GameObject");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey (KeyCode.L)) {
+			cube.transform.localPosition= new Vector3 (cube.transform.position.x+1,cube.transform.position.y,cube.transform.position.z);
+		}
+
+		if (Input.GetKey (KeyCode.R)) {
+			cube.transform.localPosition= new Vector3 (cube.transform.position.x-1,cube.transform.position.y,cube.transform.position.z);
+		}
+
+	}
+}
